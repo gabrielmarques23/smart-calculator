b7e00697183fa90d105fda12ad2e758138ced54c
Gabriel Marques <gabrielmarques.if@gmail.com>
2024-05-14 18:33:36 -0300

    Adicionei função para cada operador

--- js/main.js
+++ js/main.js
@@ -1,55 +1,73 @@
 
-//const numberToCalc = document.querySelector('span#calcDisplay');
-
 let screen = document.querySelector('#calcDisplay'); //Ok
 let deleteButton = document.querySelector('.deleteButton button');
 let cleanButton = document.querySelector('.cleanButton');
 let numberButton = document.querySelectorAll('.numberButton');
-let selectedNumbers = '';
 
-//Click in the number buttons
+let selectedNumbers = '';
+let num1 = '';
+let num2 = '';
+let operator = '';
+
+function addition() {
+    operator = '+';
+}
+function subtraction() {
+    operator = '-';
+}
+function multiplication() {
+    operator = '*';
+}
+function division() {
+    operator = '/';
+}
+function percentual() {
+    operator = '%';
+}
+
+function clickOperatorButton(oper) {
+    operator = oper;
+    showNumbersInDisplay();
+}
 
 function clickNumberButton(num) {
-    num.textContent;
-    selectedNumbers += num;
-    screen.innerHTML = selectedNumbers;
+    if (operator === '') {
+        num1 += num;
+    } else {
+        num2 += num;
+    }
+    showNumbersInDisplay();
 }
 
 function deleteCharacter() {
-    selectedNumbers = selectedNumbers.slice(0, -1);
-    screen.textContent = selectedNumbers;
+    num1 = num1.slice(0, -1);
+    num2 = num2.slice(0, -1);
+    screen.textContent = num1, num2;
 }
 
 function cleanButtonClick() {
     screen.textContent = '0';
-    selectedNumbers = '';
+    num1 = '';
+    num2 = '';
+    operator = '';
 }
 
-
+function showNumbersInDisplay() {
+    let displayText = '';
+    // Verifica se num1 e operator estão definidos
+    if (num1 !== '' && operator !== '') {
+        displayText += num1 + operator;
+    } else {
+        displayText += num1; // Se não, apenas exibe num1
+    }
+    // Verifica se num2 está definido
+    if (num2 !== '') {
+        displayText += num2;
+    }
+    screen.innerHTML = displayText;
+}
 
 
 /*
-
-let num1;
-let num2;
-
-let calc = {
-    add: (num1, num2) => {
-        return num1 + num2;
-    },
-    sub: (num1, num2) => {
-        return num1 - num2;
-    },
-    div: (num1, num2) => {
-        return num1 / num2;
-    },
-    multi: (num1, num2) => {
-        return num1 * num2;
-    },
-}
-
-
-
-console.log(calc.multi(15, 5))
 
 */
