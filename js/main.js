b1470075b608a14ba91f409587e0461826177745
Gabriel Marques <gabrielmarques.if@gmail.com>
2024-05-16 20:52:25 -0300

    Script diminuido
    Usei uma função responsavel por calculos em JS.
    Numeros já podem ser mostrados na tela da calculadora.

--- js/main.js
+++ js/main.js
@@ -1,111 +1,48 @@
 
-let screen = document.querySelector('#calcDisplay'); //Ok
-let deleteButton = document.querySelector('.deleteButton button');
-let cleanButton = document.querySelector('.cleanButton');
-let numberButton = document.querySelectorAll('.numberButton');
+let screen = document.querySelector('#calcDisplay');
+let expression = '';
 
-let selectedNumbers = '';
-let num1 = '';
-let num2 = '';
-let operator = '';
-let result = '';
-
-const math = {
-    addition: (num1, num2) => {
-        operator = '+';
-        return num1 + num2;
-    },
-    subtraction: (num1, num2) => {
-        operator = '-';
-        return num1 - num2;
-    },
-    multiplication: (num1, num2) => {
-        operator = '&#215;';
-        return num1 * num2;
-    },
-    division: (num1, num2) => {
-        operator = '&#247;';
-        let result = num1 / num2;
-        return result.toFixed(2);
-    },
-    percentual: (num1, num2) => {
-        operator = '%';
-        let numbPer2 = num2 / 100;
-        let result = num1 * numbPer2;
-        return result.toFixed(2);
-    }
-}
-
-const { addition, subtraction, multiplication, division, percentual } = math;
+/*
+Montar uma lógica para cada um dos operadores que irão fazer o calculo.
+Para cada operador fazer uma função.
+Tentar colocar o código de cada operador, retornando a expressão correta.
+*/
 
 function resultButton() {
-    switch (operator) {
-        case '+':
-            result = addition(Number(num1), Number(num2));
-            break;
-        case '-':
-            result = subtraction(Number(num1), Number(num2));
-            break;
-        case '&#215;':
-            result = multiplication(Number(num1), Number(num2));
-            break;
-        case '&#247;':
-            result = division(Number(num1), Number(num2));
-            break;
-        case '%':
-            result = percentual(Number(num1), Number(num2));
-            break;
-        default:
-            result = '';
+    if (expression !== '') {
+        let result = eval(expression);
+        screen.textContent = result;
+        expression = result.toString();
     }
-    screen.textContent = result;
 }
 
 function clickOperatorButton(oper) {
-    operator = oper;
-    showNumbersInDisplay();
+    if (expression !== '') {
+        if (!expression.match(/[+\-*/]$/)) {
+            expression += oper;
+            showExpression();
+        }
+    }
 }
 
 function clickNumberButton(num) {
-    if (operator === '') {
-        num1 += num;
-    } else {
-        num2 += num;
-    }
-    showNumbersInDisplay();
+    expression += num;
+    showExpression();
 }
 
 function deleteCharacter() {
-    if (num2 !== '') {
-        num2 = num2.slice(0, -1);
-    } else if (operator !== '') {
-        operator = ''
-    } else if (num1 !== '') {
-        num1 = num1.slice(0, -1);
-    }
-    showNumbersInDisplay();
+    expression = expression.slice(0, -1);
+    showExpression();
 }
 
 function cleanButtonClick() {
     screen.textContent = '0';
-    num1 = '';
-    num2 = '';
-    operator = '';
+    expression = '';
 }
 
-function showNumbersInDisplay() {
-    let displayText = '';
-
-    if (num1 !== '' && operator !== '') {   // Verifica se num1 e operator estão definidos
-        displayText += num1 + operator;
-    } else {                                // Se não, apenas exibe num1
-        displayText += num1;
-    }
-
-    if (num2 !== '') {                      // Verifica se num2 está definido
-        displayText += num2;
-    }
-    screen.innerHTML = displayText;
+function showExpression() {
+    let expressionWithSpaces = expression.replace(/([+\-*/])/g, ' $1 ')
+    screen.textContent = expressionWithSpaces.trim() || '0';
 }
 
 
