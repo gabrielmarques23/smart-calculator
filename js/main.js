8a404700ada7f69534ad9b09062b5c74bb5a5972
Gabriel Marques <gabrielmarques.if@gmail.com>
2024-05-15 22:05:44 -0300

    Adicionado a função para fazer os calculos

--- js/main.js
+++ js/main.js
@@ -8,21 +8,57 @@ let selectedNumbers = '';
 let num1 = '';
 let num2 = '';
 let operator = '';
+let result = '';
 
-function addition() {
-    operator = '+';
+const math = {
+    addition: (num1, num2) => {
+        operator = '+';
+        return num1 + num2;
+    },
+    subtraction: (num1, num2) => {
+        operator = '-';
+        return num1 - num2;
+    },
+    multiplication: (num1, num2) => {
+        operator = '&#215;';
+        return num1 * num2;
+    },
+    division: (num1, num2) => {
+        operator = '&#247;';
+        let result = num1 / num2;
+        return result.toFixed(2);
+    },
+    percentual: (num1, num2) => {
+        operator = '%';
+        let numbPer2 = num2 / 100;
+        let result = num1 * numbPer2;
+        return result.toFixed(2);
+    }
 }
-function subtraction() {
-    operator = '-';
-}
-function multiplication() {
-    operator = '*';
-}
-function division() {
-    operator = '/';
-}
-function percentual() {
-    operator = '%';
+
+const { addition, subtraction, multiplication, division, percentual } = math;
+
+function resultButton() {
+    switch (operator) {
+        case '+':
+            result = addition(Number(num1), Number(num2));
+            break;
+        case '-':
+            result = subtraction(Number(num1), Number(num2));
+            break;
+        case '&#215;':
+            result = multiplication(Number(num1), Number(num2));
+            break;
+        case '&#247;':
+            result = division(Number(num1), Number(num2));
+            break;
+        case '%':
+            result = percentual(Number(num1), Number(num2));
+            break;
+        default:
+            result = '';
+    }
+    screen.textContent = result;
 }
 
 function clickOperatorButton(oper) {
@@ -40,9 +76,14 @@ function clickNumberButton(num) {
 }
 
 function deleteCharacter() {
-    num1 = num1.slice(0, -1);
-    num2 = num2.slice(0, -1);
-    screen.textContent = num1, num2;
+    if (num2 !== '') {
+        num2 = num2.slice(0, -1);
+    } else if (operator !== '') {
+        operator = ''
+    } else if (num1 !== '') {
+        num1 = num1.slice(0, -1);
+    }
+    showNumbersInDisplay();
 }
 
 function cleanButtonClick() {
@@ -54,18 +95,20 @@ function cleanButtonClick() {
 
 function showNumbersInDisplay() {
     let displayText = '';
-    // Verifica se num1 e operator estão definidos
-    if (num1 !== '' && operator !== '') {
+
+    if (num1 !== '' && operator !== '') {   // Verifica se num1 e operator estão definidos
         displayText += num1 + operator;
-    } else {
-        displayText += num1; // Se não, apenas exibe num1
+    } else {                                // Se não, apenas exibe num1
+        displayText += num1;
     }
-    // Verifica se num2 está definido
-    if (num2 !== '') {
+
+    if (num2 !== '') {                      // Verifica se num2 está definido
         displayText += num2;
     }
     screen.innerHTML = displayText;
 }
+
+
 
 
 /*
