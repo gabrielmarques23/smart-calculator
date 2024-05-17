85041b7382b079ed67aa36160926835b1a39c679
Gabriel Marques <gabrielmarques.if@gmail.com>
2024-05-17 19:08:20 -0300

    Script atualizado
    Adicionei funções, para finalmente conseguir calcular expressões mais complexas.

--- js/main.js
+++ js/main.js
@@ -1,53 +1,72 @@
 
 let screen = document.querySelector('#calcDisplay');
+let displayExpression = '';
 let expression = '';
 
-/*
-Montar uma lógica para cada um dos operadores que irão fazer o calculo.
-Para cada operador fazer uma função.
-Tentar colocar o código de cada operador, retornando a expressão correta.
-*/
-
-function resultButton() {
-    if (expression !== '') {
-        let result = eval(expression);
-        screen.textContent = result;
-        expression = result.toString();
-    }
+function updateExpression() {
+    screen.innerHTML = displayExpression || '0'
 }
 
-function clickOperatorButton(oper) {
-    if (expression !== '') {
-        if (!expression.match(/[+\-*/]$/)) {
-            expression += oper;
-            showExpression();
-        }
-    }
+function addition() {
+    expression += '+';
+    displayExpression += '+';
+    updateExpression()
+}
+function subtraction() {
+    expression += '-';
+    displayExpression += '-';
+    updateExpression()
+}
+function multiplication() {
+    expression += '*';
+    displayExpression += '×';
+    updateExpression()
+}
+function division() {
+    expression += '/';
+    displayExpression += `÷`;
+    updateExpression()
+}
+function percentual() {
+    expression += '/100';
+    displayExpression += `%`;
+    updateExpression()
+}
+function decimal() {
+    expression += '.'
+    displayExpression += '.';
+    updateExpression()
 }
 
 function clickNumberButton(num) {
     expression += num;
-    showExpression();
+    displayExpression += num;
+    updateExpression();
 }
 
 function deleteCharacter() {
     expression = expression.slice(0, -1);
-    showExpression();
+    displayExpression = displayExpression.slice(0, -1);
+    updateExpression();
 }
 
 function cleanButtonClick() {
     screen.textContent = '0';
+    displayExpression = '';
     expression = '';
 }
 
-function showExpression() {
-    let expressionWithSpaces = expression.replace(/([+\-*/])/g, ' $1 ')
-    screen.textContent = expressionWithSpaces.trim() || '0';
+function resultButton() {
+    if (expression !== '') {
+        try {
+            let result = eval(expression.replace(/(\d+)%/g, ' $1/100 '));
+            screen.textContent = result;
+            expression = result.toString();
+            displayExpression = expression;
+        } catch (e) {
+            screen.textContent = 'Error';
+            expression = '';
+            displayExpression = '';
+        }
+    }
 }
-
-
-
-
-/*
-
-*/
